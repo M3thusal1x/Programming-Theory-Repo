a878acbb8e811fe729c4b9c748d923a39948b1ac
M3thusal1x <117019949+M3thusal1x@users.noreply.github.com>
2022-11-01 18:58:26 +0100

    Animal class
    - First version of an abstract animal class implemented.

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Abstract parent class for all animals.
+public abstract class Animal : MonoBehaviour
+{
+    private float timeSinceSpawn = 0.0f;
+
+    // internal Name
+    private string m_Name = "Animal";
+    // Animal name. Name length may not exceed 10 characters.
+    public string Name
+    {
+        get { return m_Name; }
+        set { 
+            if (Name.Length > 10)
+            {
+                Debug.Log("You can't use animal names with more than 10 characters!");
+            }
+            else
+            {
+                m_Name = Name;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        // Demo movement
+
+        
+    }
+
+    // Animal speed
+    public float Speed { get; set; }
+
+    public abstract void move();
+
+    public abstract void jump();
+
+    // Spawn animal at specified position
+    public abstract void spawn(Vector3 position);
+
+    // Spawn animal at origin
+    public void spawn()
+    {
+        spawn(Vector3.zero);
+    }
+}
