6a13fada5e162c372fc03b33ba61869e6c8f7f02
M3thusal1x <117019949+M3thusal1x@users.noreply.github.com>
2022-11-02 22:10:41 +0100

    Quit Button
    - added quit button and its functionality

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
+using UnityEditor;
 
 
 public class MainManager : MonoBehaviour
@@ -70,5 +69,14 @@ public class MainManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
